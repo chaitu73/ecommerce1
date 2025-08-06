4a984de9993a6e4655a235a9969e954001de7143
chaitu73 <163741933+chaitu73@users.noreply.github.com>
2025-08-06 20:26:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+   <BrowserRouter basename="/ecommerce1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
@@ -40,7 +40,7 @@ function App() {
           <Route path="orders" element={<Orders />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
     </>
   )
 }
